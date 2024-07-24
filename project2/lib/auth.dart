a846a046316789b7eac9d3e5277567acd8060a2d
CameronTedder <camtedder10@gmail.com>
2024-07-23 23:30:27 -0400

    Fix typo in auth.dart

--- project2/lib/auth.dart
+++ project2/lib/auth.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:project2/homepage.dart';git 
+import 'package:project2/homepage.dart'; 
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final FirebaseFirestore _db = FirebaseFirestore.instance;
